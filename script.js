0de77b9a4000e06b54d8ac81d82c562d972bc02c
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-30 23:41:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function curry(callback) {
-  
+  return function curried(...args) {
+    if (args.length === 0) {
+      return callback();
+    } else {
+      return function(...nextArgs) {
+        if (nextArgs.length === 0) {
+          return callback(...args);
+        } else {
+          return curried(...args, ...nextArgs);
+        }
+      };
+    }
+  };
 }
 
 module.export=curry
